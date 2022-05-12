3f41e5d39ee3f66f33e7082da228bc0048076d00
Kijung Park <kjpark4321@naver.com>
2022-05-12 20:53:07 +0900

    [chore] fix run_container.sh

--- service/backend/run_container.sh
+++ service/backend/run_container.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 STOP_RESULT=$(docker ps -aqf "name=^santaboj-backend")
-if [ "$STOP_RESULT" != ""]; then
+if [ -n $STOP_RESULT]; then
   docker stop $(docker ps -aqf "name=^santaboj-backend")
 fi
 RM_RESULT=$(docker ps -aqf "name=^santaboj-backend")
-if [ "$RM_RESULT" != ""]; then
+if [ -n $RM_RESULT ]; then
   docker rm $(docker ps -aqf "name=^santaboj-backend")
 fi
 
