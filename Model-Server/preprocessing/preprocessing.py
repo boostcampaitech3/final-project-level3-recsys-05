cbdf1f7b371065d70406dd5e455704a37d05f78d
SeongBeomLEE <2712qwer@naver.com>
2022-05-12 06:32:45 +0000

    chore: path 수정

--- Model-Server/preprocessing/preprocessing.py
+++ Model-Server/preprocessing/preprocessing.py
@@ -1,11 +1,10 @@
 import json
 
-with open('/opt/ml/level-3-final-project/Model-Server/preprocessing/problem_id2idx.json', 'r', encoding = 'utf-8') as f:
+with open('/opt/ml/final-project-level3-recsys-05/Model-Server/preprocessing/problem_id2idx.json', 'r', encoding = 'utf-8') as f:
     problem_id2idx = json.load(f)
 
-with open('/opt/ml/level-3-final-project/Model-Server/preprocessing/idx2problem_id.json', 'r', encoding = 'utf-8') as f:
+with open('/opt/ml/final-project-level3-recsys-05/Model-Server/preprocessing/idx2problem_id.json', 'r', encoding = 'utf-8') as f:
     idx2problem_id = json.load(f)
-
 
 def preprocessing_seq_problem_id2idx(problem_seq):
     return_problem_seq = []
