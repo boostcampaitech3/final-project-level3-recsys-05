ee39aba60f0d0f0f042d74556b3de5ea2eba74c1
Kijung Park <kjpark4321@naver.com>
2022-05-12 16:57:02 +0900

    [fix] fix settings.py

--- service/backend/santaBOJ/settings.py
+++ service/backend/santaBOJ/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'santaBOJ.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
