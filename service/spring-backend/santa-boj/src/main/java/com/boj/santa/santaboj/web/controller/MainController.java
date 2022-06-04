52aa5999e7c51d509a628291268dc83a109c08e7
Kijung Park <kjpark4321@naver.com>
2022-06-04 20:16:26 +0900

    [fix] newbie 페이지 로그인 정보 Model 로 넘기기

--- service/spring-backend/santa-boj/src/main/java/com/boj/santa/santaboj/web/controller/MainController.java
+++ service/spring-backend/santa-boj/src/main/java/com/boj/santa/santaboj/web/controller/MainController.java
@@ -1,5 +1,7 @@
 package com.boj.santa.santaboj.web.controller;
 
+import com.boj.santa.santaboj.domain.entity.Member;
+import com.boj.santa.santaboj.domain.service.MemberService;
 import com.boj.santa.santaboj.web.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 @Controller
 public class MainController {
+
+    private final MemberService memberService;
 
     @GetMapping("/")
     public String index(Model model){
@@ -23,7 +27,11 @@ public class MainController {
     }
 
     @GetMapping("/newbie")
-    public String newbie(){
+    public String newbie(Model model){
+
+        Member authMember = memberService.findMemberByAuthentication();
+        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("principal", principal);
         return "newbie";
     }
 
